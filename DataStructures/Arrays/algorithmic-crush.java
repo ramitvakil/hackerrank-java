345738c25bab49f74238a08498f4e05070b99923
Ramit Vakil <ramitvakil@users.noreply.github.com>
2017-06-08 19:17:25 +0530

    Algorithmic crush problem solution
    this is the solution for algorithmic crush solution.

---
+++ DataStructures/Arrays/algorithmic-crush.java
@@ -0,0 +1,42 @@
+/**
+https://www.hackerrank.com/challenges/crush 
+**/
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int N, M, a, b;
+    long k; // int k;
+    Scanner in = new Scanner(System.in);
+    N = in.nextInt();
+    M = in.nextInt();
+    long[] differenceArray = new long[N+1]; // int[] ...
+    for (int i=0; i<M; i++) 
+    {
+        in.nextLine();
+        a = in.nextInt(); 
+        b = in.nextInt();
+        k = in.nextLong(); // in.nextInt();
+        differenceArray[a] += k;
+        if (b+1<=N)
+            differenceArray [b+1] -= k;
+    }
+
+    long max = 0, addedDifference = 0; // int
+    for (int i=1; i<=N; i++) 
+    {
+        addedDifference = addedDifference + differenceArray[i];
+        if (max < addedDifference)
+            max = addedDifference;
+    }
+    System.out.println(max);
+    in.close();
+
+    }
+}
